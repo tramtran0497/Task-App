df4020a5187995bdec40ee0ae5a876ca5b9a864f
tramtran0497 <tramtran0497@gmail.com>
2022-04-27 12:22:03 +0300

    Create post method to get data from users using postman

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,12 +6,3 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
     useUnifiedTopology: true, 
 });
 
-
-// const user1 = new User({
-//     name: "Tram",
-//     email: "tram@gmail.com",
-//     phone: 12345890,
-//     password: "tram3",
-// });
-
-// user1.save().then(() => console.log(user1)).catch(err => console.log(err));
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
-require('dotenv').config()
+require('dotenv').config();
+const User = require("./models/User");
+require("./db/mongoose");
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.json());
+
+app.post("/task", (req, res) => {
+    const user1 = new User(req.body);
+    res.send(user1);
+});
 
 app.listen(port, () => {
     console.log(`Server is up ${port}`)
