d1a1fe33904ea9f95ac8ee2e3bf8d648ad49bf22
tramtran0497 <tramtran0497@gmail.com>
2022-05-12 11:22:08 +0300

    replace dotenv by env-cmd

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const User = require("../models/User");
 // Connect MongoDB Cloud
-mongoose.connect(`mongodb+srv://tramtran0497:${process.env.PASSWORD}@cluster0.ogj7n.mongodb.net/TaskApp?retryWrites=true&w=majority`)
-    .then(() => console.log("Database is connected!"))
-    .catch(err => console.log("Database connects error!", err))
+// mongoose.connect(`mongodb+srv://tramtran0497:${process.env.PASSWORD}@cluster0.ogj7n.mongodb.net/TaskApp?retryWrites=true&w=majority`)
+//     .then(() => console.log("Database is connected!"))
+//     .catch(err => console.log("Database connects error!", err))
 
-// mongoose.connect(process.env.MONGODB_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true, 
-// });
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, 
+});
 
